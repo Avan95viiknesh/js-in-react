60e129e82cb3680f8a25331eb69b6f9ce53b0f47
Avan95viiknesh <vignesh.murugan@avaniko.com>
2022-11-15 18:03:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React,{useState  } from 'react';
 import './App.css';
 
 function App() {
+ 
+
+  const [index, setIndex] = useState(0);
+
+ 
+  const change = ['primary', 'success', 'warning', 'danger' ]
+
+
+const random = change[Math.floor(Math.random() * change.length)]
+
+ console.log(random);
+
+
+const textColor =() =>  {
+  switch(random) {
+    case 'primary': 
+      return 'primary';
+    case 'success': 
+      return 'success'
+    case 'warning': 
+      return 'warning'
+    case 'danger': 
+      return 'danger'   
+    default: 
+     
+  }
+}
+
+
+ const addElement= ( ) => {
+ setIndex(index +1)
+  
+ }
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <div className="App">
+
+
+
+  <button  onClick={addElement} >get item</button>  
+  <h1 > the color of <span className= {textColor()}> {random} </span>  </h1> 
+  
+
+
+
+
+
+ 
+</div>
+    </>
+  
   );
 }
 
